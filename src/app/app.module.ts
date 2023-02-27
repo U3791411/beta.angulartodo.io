a860701a4b6e93440228d8923a0d70eaae7cca06
U3791411 <softwaredeveloper.project@outlook.com>
2023-02-27 21:34:10 +0000

    BUILD: Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './userinterface/navigation/navigation.component';
+import { FooterComponent } from './userinterface/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
