926ac4a6f651c8eb243fd99b3d6e007d3e900c08
U3791411 <softwaredeveloper.project@outlook.com>
2023-03-02 01:09:00 +0000

    BUILD: Typescript Interface • Todo.ts

---
+++ src/app/Todo.ts
@@ -0,0 +1,7 @@
+/* [TypeScript] Interface | [Optional] Flag - id?
+Note: (id) not initialized until saved in JSON Server */
+export interface Todo {
+  id?: number;
+  text: string;
+  important: boolean;
+}
